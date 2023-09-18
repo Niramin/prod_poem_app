20b9e133c18fba61ba3c9fd6e506e9e1a08c30b1
Shashwat <shashwatratna@gmail.com>
2023-09-18 16:42:51 +0530

    poem content display working with bottom 2 buttons

--- lib/controller/controller.dart
+++ lib/controller/controller.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:prod_poem_app/model/PoemRepository.dart';
+import 'package:prod_poem_app/model/poem.dart';
+import 'package:intl/intl.dart' as intl;
 
 class Controller extends StatefulWidget{
   const Controller({super.key});
@@ -9,23 +12,86 @@ class Controller extends StatefulWidget{
 
 
 class _ControllerState extends State<Controller>{
-  //get the day's Poem
-  //get the curent sky 
-  
+
+  bool isLike = false;
+  //for getting the day's poem
+  IPoemRepository poems = Poems();
+  Poem todayPoem = Poem();
+
+
+  //for getting the day's sky
+
+
+
+ @override
+ void initState() 
+ {
+    super.initState();
+    firstTimeLoad();
+ }
+ Future<bool> firstTimeLoad() async{
+    var tempPoem = await poems.getTodaysPoemAsync();
+    setState(() {
+      todayPoem = tempPoem;
+      isLike = todayPoem.isLiked;
+    });
+    return true;
+ }
 
  @override
  Widget build(BuildContext context)
  {
-  return MaterialApp(
-      title: 'Daily Quotes',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
-        useMaterial3: true,
-      ),
-      home: Text(
-        "Hello",
-      ),
-    );
+    var dayOfWeek = intl.DateFormat('EEEE').format(DateTime.now());
+    IconData icon;
+    if (isLike) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+  return Scaffold(
+    backgroundColor: Colors.blue.shade200,
+    appBar: AppBar(
+          backgroundColor: Colors.pink.shade100,
+          title: Center(
+              child: Text(
+            dayOfWeek,
+            style: const TextStyle(
+              fontFamily: 'MedievalSharp',
+              fontWeight: FontWeight.bold,
+              color: Colors.limeAccent,
+              fontSize: 40,
+            ),
+          )),
+        ),
+    body: Text(todayPoem.content),
+    floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FloatingActionButton(
+              onPressed: () {
+                setState(() {
+                  isLike = !isLike;
+                });
+              },
+              tooltip: 'Like',
+              backgroundColor: Colors.pink.shade100,
+              foregroundColor: Colors.limeAccent,
+              child: Icon(icon),
+            ),
+            const SizedBox(
+              width: 70,
+            ),
+            FloatingActionButton(
+              onPressed: () => {},
+              tooltip: 'Shuffle',
+              backgroundColor: Colors.pink.shade100,
+              foregroundColor: Colors.limeAccent,
+              child: const Icon(Icons.shuffle),
+            ),
+          ],
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+  );
  }
 
 }
